3d949ed5b8a94bc2aa5b7ff7465c9fb25162f4ec
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-03-10 17:09:49 +0430

    trasfer data from app js to reg page

--- backend.js
+++ backend.js
@@ -9,14 +9,8 @@ if (isLogin) {
   app.get("/", (req, res) => {
     res.render("index");
   });
-  app.get("/login", (req, res) => {
-    res.render("login");
-  });
   app.get("/watchList", (req, res) => {
     res.render("watchList");
-  });
-  app.get("/register", (req, res) => {
-    res.render("register");
   });
   app.use((req, res) => {
     res.status(404).render("404");
