b4b614e3f75fd29f21e01b84c15006396f146ae6
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-03-13 22:05:12 +0430

    add to watchlist fanc

--- backend.js
+++ backend.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-let isLogin = true;
+let isLogin = false;
 app.listen(3000);
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
